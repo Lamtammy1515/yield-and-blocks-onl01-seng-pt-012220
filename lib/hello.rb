94f762029c6121a5f73a1f5204187de5d0273c44
Tammy Lam <lamtammy1515@gmail.com>
2020-01-28 22:24:54 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,18 @@
-def hello_t
-
+def hello_t(name)
+  if block_given?
+    i = 0
+ 
+    while i < name.length
+      yield(name[i])
+      i = i + 1
+    end
+ 
+    name
+  else
+    puts "Hey! No block was given!"
+  end
 end
+
 
 # call your method here!
 
